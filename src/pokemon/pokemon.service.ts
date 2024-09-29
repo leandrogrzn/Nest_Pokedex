b8c63107120b69c524080df6aff51271683b33a6
Leandro Garzon <hekoej@gmail.com>
2024-09-28 20:21:06 -0500

    Insertar pokemons por lote

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -12,7 +12,7 @@ export class PokemonService {
   constructor(
 
     @InjectModel( Pokemon.name )
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
 
   ) {}
 
