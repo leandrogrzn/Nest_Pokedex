005cbb2c8a7ad32a8ea97b181e487a52b39c444e
Leandro Garzon <hekoej@gmail.com>
2024-09-30 21:48:50 -0500

    Configuracion de variables de entorno, Configuracion Loader, ConfigurationService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,25 @@
 import { join } from 'path'; //Node
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { PokemonModule } from './pokemon/pokemon.module';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { EnvConfiguration } from './config/env.config';
 
 @Module({
   imports: [
+
+    ConfigModule.forRoot({
+      load: [ EnvConfiguration ],
+    }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
 
     PokemonModule,
 
@@ -22,3 +29,4 @@ import { SeedModule } from './seed/seed.module';
   ],
 })
 export class AppModule {}
+
