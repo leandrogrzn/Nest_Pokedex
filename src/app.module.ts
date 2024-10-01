3f2e0f0c30b275bb4886f12c6142276520ebc2d2
Leandro Garzon <hekoej@gmail.com>
2024-10-01 14:45:49 -0500

    joi - ValidationSchema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,19 @@ import { join } from 'path'; //Node
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, Schema } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
 
     ConfigModule.forRoot({
       load: [ EnvConfiguration ],
+      validationSchema: JoiValidationSchema,
     }),
 
     ServeStaticModule.forRoot({
